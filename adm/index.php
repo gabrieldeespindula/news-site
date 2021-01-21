0302ac06a6171df7d6efb6b39c2bbdb16b70a1c9
Gabriel de Espindula <gabrieldeespindula80@gmail.com>
2021-01-20 21:05:47 -0300

    adm

---
+++ adm/index.php
@@ -0,0 +1,99 @@
+<?php
+// connecting
+require_once '../database/dbconnectlogin.php';
+
+// sessions
+session_start();
+
+
+// send button
+if(isset($_POST['button-login'])):
+    $errors = array();
+    $login = mysqli_escape_string($connectlogin, $_POST['login']);
+    $password = mysqli_escape_string($connectlogin, $_POST['password']);
+    
+    if (empty($login) or empty($password)):
+        $errors[] = "<li> The login / password field needs to be filled. </li>";
+    else:
+        $sql = "SELECT login FROM news WHERE login = '$login'";
+        $result = mysqli_query($connectlogin, $sql);
+
+        if(mysqli_num_rows($result) > 0):
+            $sql = "SELECT * FROM news WHERE login = '$login'";
+            $result = mysqli_query($connectlogin, $sql);
+            $data = mysqli_fetch_array($result);
+            mysqli_close($connectlogin);
+
+
+            if(password_verify($password, $data['password'])):
+                $_SESSION['logged'] = true;
+                $_SESSION['id_user'] = $data['Id'];
+                header('location: read.php');
+            else:
+                $errors[] = "<li> Senha incorreta. </li>";
+            endif;
+        else:
+            $errors[] = "<li> Usuário não encontrado.</li>";
+        endif;
+    endif;
+
+endif;
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="../node_modules/bootstrap/compiler/bootstrap.css">
+        <link rel="stylesheet" href="../style/css/style.css">
+        <link rel="stylesheet" href="../node_modules/font-awesome/css/font-awesome.css">
+        <title>Login</title>
+    </head>
+
+    <body>
+        <script src="../node_modules/jquery/dist/jquery.js"></script>
+        <script src="../node_modules/popper.js/dist/umd/popper.js"></script>
+        <script src="../node_modules/bootstrap/dist/js/bootstrap.js"></script>
+
+        <div class="container">
+            <div class="row">
+                <div class="col-12 text-center my-5">
+                    <h1 class="">Login</h1>
+                </div>
+            </div>
+            <div class="row justify-content-center">
+                <?php
+                if(!empty($errors)):
+                    foreach($errors as $error):
+                        echo '<h6>'.$error.'</h6>';
+                    endforeach;
+                endif;
+                ?>
+            </div>
+            <div class="row justify-content-center mb-5">
+                <div class="col-sm-12 col-md-10 col-lg-8">
+                    <div>
+                        <form action="<?php $_SERVER['PHP_SELF'] ?>" method="POST">
+                            <div class="form-row">
+                                <div class="form-group col-sm-6">
+                                    <label for="inputLogin">Login</label>
+                                    <input type="text" class="form-control" id="inputLogin"  name="login">
+                                </div>
+                                <div class="form-group col-sm-6">
+                                    <label for="inputPassword">Senha:</label>
+                                    <input type="password" name="password" class="form-control" id="inputSobrenome">
+                                </div>
+                            </div>
+                            <div class="form-row">
+                                <div class="form-group col-sm-12">
+                                    <button type="submit" class="btn btn-danger" name="button-login">Entrar</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+    </body>
+</html>
--- adm/read.php
+++ adm/read.php
@@ -8,7 +8,7 @@ session_start();
 
 // verification
 if(!isset($_SESSION['logged'])):
-    header ('Location: adm.php');
+    header ('Location: index.php');
 endif;
 
 //data
@@ -24,45 +24,66 @@ mysqli_close($connectlogin);
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="../node_modules/bootstrap/compiler/bootstrap.css">
+        <link rel="stylesheet" href="../style/css/style.css">
+        <link rel="stylesheet" href="../node_modules/font-awesome/css/font-awesome.css">
         <title>Read</title>
 
     </head>
     <body>
-        <header>
-            <?php 
-            if (count($_SESSION) > 2): 
-                if($_SESSION['msg'] != "0"):
-            ?>
-            <p> <?php echo $_SESSION['msg']; ?></p>
-            <?php $_SESSION['msg'] = "0";
-            endif;
-            endif; ?>
-
-            <h1>Notícias no Banco de Dados</h1>
-            <a href="../database/logout.php">SAIR</a>
-            <form action="buscaread.php" method="POST">
-                <input placeholder="Busque Notícias" type="text" name="busca" />
-                <button type="submit" >Buscar</button>
-            </form>
-        </header>
+        <script src="../node_modules/jquery/dist/jquery.js"></script>
+        <script src="../node_modules/popper.js/dist/umd/popper.js"></script>
+        <script src="../node_modules/bootstrap/dist/js/bootstrap.js"></script>
+        <?php 
+        if (count($_SESSION) > 2): 
+            if($_SESSION['msg'] != "0"):
+        ?>
+        <p> <?php echo $_SESSION['msg']; ?></p>
+        <?php $_SESSION['msg'] = "0";
+        endif;
+        endif; ?>
+        <div class="my-3 w-100 row justify-content-center">
+            <a class="btn btn-outline-info" href="../database/logout.php">SAIR</a>
+        </div>
         <main>
-
+        <div class="col-md-12 w-100">
+            <div class="row justify-content-center">
+                <div class="col-md-4 my-auto">
+                    <div class="row justify-content-center">
+                        <form action="buscaread.php" class="form-inline" method="POST">
+                            <input placeholder="Busque Notícias" type="search" class="form-control" name="busca" />
+                            <button type="submit" class="ml-2 btn btn-dark" >Buscar</button>
+                        </form>
+                    </div>
+                </div>
+                <div class="col-md-4 my-auto">
+                    <h1 class="text-center"><i class="fa fa-newspaper-o text-danger" aria-hidden="true"></i>  Notícias no Banco de Dados</h1>
+                </div>
+                <div class="col-md-4 my-auto">
+                    <div class="row justify-content-center">
+                        <a href="add.php"><button class="btn btn-outline-danger">Adicionar</button></a>
+                    </div>
+                </div>
+            </div>
+            <div class="row w-100">
             <?php
             $sql = "SELECT * FROM news ORDER BY Id DESC";
             $result = mysqli_query($connectnews, $sql);
 
             while ($data = mysqli_fetch_array($result)):
             ?>
-
-            <div>
-                <a href="edit.php?id=<?php echo $data['Id']; ?>"><p><?php echo $data['titulo']; ?></p></a>
+                <div class="card mx-auto my-5">
+                    <div class="card-body text-center">
+                        <a href="edit.php?id=<?php echo $data['Id'] ?>"><?php echo '<img class="rounded img-fluid" height="250px" width="250px" src=data:image;base64,'.$data['img'].' />'; ?></a>
+                    </div>
+                    <div class="card-body text-center">
+                        <a class="h6 card-text" href="edit.php?id=<?php echo $data['Id'] ?>"><?php echo $data['titulo'] ?></a>
+                        <h6 class="card-subtitle my-2 text-muted"><?php echo $data['data'] ?><h6>
+                    </div>
+                </div>
+                <?php
+                endwhile; ?>
             </div>
-
-            <?php endwhile; ?>
-        </main>
-        <div>
-            <a href="add.php"><button class="button">ADICIONE NOVAS NOTÍCIAS</button></a>
         </div>
-
     </body>
 </html>
